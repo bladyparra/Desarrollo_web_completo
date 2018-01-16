5dc3e1aa45c1f8c9b231c8ec917a502af33e9908
Bladimir Gerson Parra Bermudez <ipproyectossoluciones@gmail.com>
2018-01-16 18:16:14 -0500

    Se configura la sección "programa" para que funcione de manera correcta con jquery, css

--- js/main.js
+++ js/main.js
@@ -165,3 +165,17 @@ function initMap() {
         
     });//DOM CONTENT LOADED
 })();
+
+$(function(){
+    //Programa de conferencias
+    $('.programa-evento .info-curso:first').show();
+    $('.menu-programa a:first').addClass('activo');
+    $('.menu-programa a').on('click', function(){
+        $('.menu-programa a').removeClass('activo');
+        $(this).addClass('activo');
+        $('.ocultar').hide();
+        var enlace = $(this).attr('href');
+        $(enlace).fadeIn(1000);
+        return false;
+    })
+});

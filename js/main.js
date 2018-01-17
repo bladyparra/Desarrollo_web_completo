a1181d26fadc998c8ff09d7e57f56769dc3d5635
Bladimir Gerson Parra Bermudez <ipproyectossoluciones@gmail.com>
2018-01-16 20:00:29 -0500

    Se agrega código para las secciones "resumen-evento" y "cuenta-regresiva" utilizando los plugins de JQuery "animateNumber" y "countdown"

--- js/main.js
+++ js/main.js
@@ -177,5 +177,26 @@ $(function(){
         var enlace = $(this).attr('href');
         $(enlace).fadeIn(1000);
         return false;
-    })
+    });
+
+    //Animaciones para los números de .resumen-evento
+    $('.resumen-evento li:nth-child(1) p').animateNumber({number: 6}, 1200);
+    $('.resumen-evento li:nth-child(2) p').animateNumber({number: 15}, 1200);
+    $('.resumen-evento li:nth-child(3) p').animateNumber({number: 3}, 1500);
+    $('.resumen-evento li:nth-child(4) p').animateNumber({number: 9}, 1500);
+
+    //Animaciones para los números de .cuenta-regresiva
+    $('.cuenta-regresiva').countdown('2018/12/10 09:00:00', function(event) {
+        $('#dias').html(event.strftime('%D'));
+        $('#horas').html(event.strftime('%H'));
+        $('#minutos').html(event.strftime('%M'));
+        $('#segundos').html(event.strftime('%S'));
+
+
+    
+        
+    });
+
+
+
 });

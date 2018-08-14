2fdc1c647039016581b21db7d4dd5c6b0dd2b992
Bladimir Gerson Parra Bermudez <ipproyectossoluciones@gmail.com>
2018-08-14 17:37:39 -0500

    Se agrega UTF8 a la conexión a la BD

--- includes/funciones/db_conexion.php
+++ includes/funciones/db_conexion.php
@@ -1,6 +1,7 @@
 <?php
 
     $conn = new mysqli('localhost', 'root', '', 'gdlwebcamp');
+    $conn->set_charset('utf8');
 
     if($conn->connect_error) {
       echo $error -> $conn->connect_error;
